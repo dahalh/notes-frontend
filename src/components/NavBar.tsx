a0d431ea2f2f7a896799a1eaff92671c6e387d5b
dahalh <dahalh@outlook.com>
2023-04-18 11:21:10 +1000

    react router dom links added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { User } from "../models/user";
 import NavBarLoggedInView from "./NavBarLoggedInView";
 import NavBarLoggedOutView from "./NavBarLoggedOutView";
+import { Link } from "react-router-dom";
 
 interface NavBarProps {
   loggedInUser: User | null;
@@ -19,9 +20,16 @@ const NavBar = ({
   return (
     <Navbar bg="primary" variant="dark" expand="sm" sticky="top">
       <Container>
-        <Navbar.Brand>Notes App</Navbar.Brand>
+        <Navbar.Brand to="/" as={Link}>
+          Notes App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link to="/privacy" as={Link}>
+              Privacy
+            </Nav.Link>
+          </Nav>
           <Nav className="ms-auto">
             {loggedInUser ? (
               <NavBarLoggedInView
